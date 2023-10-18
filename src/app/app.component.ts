1b7b8ae3a3221f941ea5fee8154ec4ea09a08d67
Olga Budova <olgabudova55@gmail.com>
2023-10-18 17:44:07 +0200

    task 3 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { evaluate } from '@cloudmark/evaluator';
+
+// I needed to redeclare that type, cz unfortunately it is not exported from the lib.
+type Result<Input, Output> = {
+  success: true;
+  value: Output;
+  rest: Input;
+  column: number;
+} | {
+  success: false;
+  reason: string;
+  column: number;
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calc';
+  expression: string = '';
+  errorMessage: string | null = null;
+  history: { expression: string; val: number }[] = [];
+  response: Result<string, number>
+  numbersArray: string[];
+
+  commandKeyMapping = {
+    delete: {key: 'DEL'},
+    clear: {key: 'AC'},
+    plus: {key: '+'},
+    minus: {key: '-'},
+    multiply: {key: '*'},
+    divide: {key: '/'},
+  }
+  constructor() {
+    this.numbersArray = Array.from({ length: 9 }, (_, index) => `${9 - index}`);
+  }
+
+  getObjectKeys(obj: Record<string, Record<string, string>>): string[] {
+    return Object.keys(obj);
+  }
+
+  onInputChange() {
+    this.errorMessage = null;
+
+    this.response = evaluate(this.expression);
+
+    if (this.response.success == false) {
+      this.errorMessage = this.response.reason;
+    }
+  }
+
+  addToHistory(expression: string, val: number) {
+    this.history.unshift({ expression, val });
+    if (this.history.length > 5) {
+      this.history.pop();
+    }
+  }
+
+  command(command) {
+    switch (command) {
+      case this.commandKeyMapping.delete.key: {
+        this.expression = this.expression.slice(0,-1);
+        break;
+      }
+      case this.commandKeyMapping.clear.key: {
+        this.expression = '';
+        break;
+      }
+    }
+
+    if (this.expression.length) {
+      this.onInputChange();
+    }
+  }
+
+  concat(symbol: string) {
+    this.expression += symbol;
+
+    this.onInputChange();
+  }
+  calculate() {
+    if (this.response.success) {
+      this.addToHistory(this.expression, this.response.value);
+      this.expression = this.response.value+'';
+      this.errorMessage = null;
+    }
+
+    if (this.response.success == false){
+      this.errorMessage = this.response.reason;
+    }
+  }
 }
